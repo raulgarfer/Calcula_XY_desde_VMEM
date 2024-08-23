7ce42ab7141a36ab8c8a05eede08ceaeda8e28e1
raul garcia <raulgarfer@gmail.com>
2024-08-23 19:22:11 +0200

    limpieza, mas comentarios

--- src/_calcula_y.s
+++ src/_calcula_y.s
@@ -1,23 +1,25 @@
 _calcula_y::
+;;0b PPLL LRRR RRRR+X XXXX
+;;P = Pagina de memoria.
+;;R = Row (Fila Y [0-24])
+;;L = Line (linea [0-7])
+;;X = coordenada X [0-79]
     ld hl,(_pvmem)    ;;direccion original
     ld de,#0xc000     ;;restamos origen inicio pantalla
     sbc hl,de         ;;guardamos
     ld (_pvmem),hl
-    ld a,#0
-    ld (y),a 
-    ld (y_l),a 
-    ld (x),a 
+    ld a,#0           
+  
 y_LINE::
-  ld a,h
-  and #0b00111000   ;;
+  ld a,h      
+  and #0b00111000   ;;cogemos los bits 
   ld h,a
+    rrca            ;;division  por 8
     rrca
     rrca
-    rrca
-     ld (y_l),a
     ld c,a
   ld hl,(_pvmem)
-  ld a,h
+  ld a,h            ;;quitamos los bits Y
   and #0b00000111
   ld h,a
   ld (_pvmem),hl
@@ -40,9 +42,6 @@ y_ROW::
     ld (y),a
   jr loop_h
 fin_h:
-  ;;ld a,l 
-  ;;or a 
-  ;;  jr z,fin_l
   ld a,l
   sub #0x50
     jr c,calcula_x
@@ -57,7 +56,6 @@ calcula_x::
   ;;0B 0000 0000 0001 1111
   ld a,l
   and #0b00011111
-  ld (x),a
   ld b,a
 fin_l:
 jr .
